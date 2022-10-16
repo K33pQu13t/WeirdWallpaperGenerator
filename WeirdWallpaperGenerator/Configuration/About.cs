272929954c2bd9028a60ee626d1a719e7328ce7d
Daniel <64601261+K33pQu13t@users.noreply.github.com>
2022-10-16 14:02:07 +0000

    build release 1.1.0 (#17)

--- WeirdWallpaperGenerator/Configuration/About.cs
+++ WeirdWallpaperGenerator/Configuration/About.cs
@@ -11,7 +11,7 @@ namespace WeirdWallpaperGenerator.Configuration
         public string Author => "K33p_Qu13t";
 
         // TODO: Update it before release:
-        public string Version { get; set; } = "1.0.2";
-        public DateTime ReleaseDate { get; set; } = new DateTime(day: 31, month: 8, year: 2022);
+        public string Version { get; set; } = "1.1.0";
+        public DateTime ReleaseDate { get; set; } = new DateTime(day: 16, month: 10, year: 2022);
     }
 }
